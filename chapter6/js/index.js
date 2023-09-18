19ddc9f11d9c2dc7948b7795d488520eb4eba379
eoqna <eoqna@naver.com>
2023-09-18 14:02:43 +0900

    페럴렉스 페이지 수강 완료

--- chapter6/js/index.js
+++ chapter6/js/index.js
@@ -4,34 +4,33 @@ const coverTitle = document.querySelector(".coverTitle");
 const coverWrap = document.querySelector(".coverWrap");
 const dimd = coverWrap.querySelector(".dimd");
 const contWrap = document.querySelector(".contWrap");
+const coverHeight = window.innerHeight;
 
 let scrollNum = 0;
 let documentHeight = 0;
 let per = 0;
-const coverHeight = window.innerHeight;
 
 coverWrap.style.height = coverHeight + "px";
 contWrap.style.marginTop = coverHeight + "px";
 
 window.addEventListener("scroll", () => {
-    scrollNum = window.scrollY;
-    documentHeight =
-        document.body.scrollHeight + coverHeight - window.innerHeight;
+  scrollNum = window.scrollY;
+  documentHeight = document.body.scrollHeight + coverHeight - window.innerHeight;
+  per = percent(scrollNum, documentHeight);
 
-    per = percent(scrollNum, documentHeight);
-    progressBar.style.width = per + "%";
+  progressBar.style.width = per + "%";
 
-    if (scrollNum >= coverHeight) {
-        header.classList.add("fixed");
-    } else {
-        //화면에서 헤더,타이틀 영역이 보일때
-        header.classList.remove("fixed");
-        coverTitle.style.top = -scrollNum / 5 + "px";
-        coverWrap.style.backgroundPosition = `center ${-scrollNum / 15}px`;
-        dimd.style.backgroundColor = `rgba(0, 0, 0, ${0.4 + scrollNum / 1000})`;
-    }
+  if( scrollHeight >= coverHeight ) {
+    header.classList.add("fixed");
+  } else {
+    // 화면에서 헤더, 타이틀 영역이 보일 때
+    header.classList.remove("fixed");
+    coverTitle.style.top = -scrollNum / 10 + "px";
+    coverWrap.style.backgroundPosition = `center ${-scrollNum / 5}px`;
+    dimd.style.backgroundColor = `rgba(0 ,0, 0, ${0.4 + scrollNum / 700})`;
+  }
 });
 
 const percent = (num, totalNum) => {
-    return ((num / totalNum) * 100).toFixed(0);
+  return (( num / totalNum ) * 100).toFixed(0);
 };
